1def82f90fa75fa73d8e12f046bb1397725fbf0e
kevinjohn01 <13521042@std.stei.itb.ac.id>
2024-12-21 22:07:23 +0700

    feat: add image segmentation

---
+++ src/utils/segment_image.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+def segment_image(image_path):
+    #Reference: https://www.analyticsvidhya.com/blog/2021/09/image-segmentation-algorithms-with-implementation-in-python/
+    
+    img = cv2.imread(image_path)
+    b, g, r = cv2.split(img)
+    rgb_img = cv2.merge([r, g, b])  # Convert BGR to RGB for display
+
+    # Convert to grayscale and apply Gaussian blur
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Apply Otsu's thresholding
+    ret, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    # Noise removal
+    kernel = np.ones((3, 3), np.uint8)
+    closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+    # Sure background area
+    sure_bg = cv2.dilate(closing, kernel, iterations=3)
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(sure_bg, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(dist_transform, 0.5 * dist_transform.max(), 255, 0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    # Marker labeling
+    ret, markers = cv2.connectedComponents(sure_fg)
+
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers + 1
+    markers[unknown == 255] = 0
+
+    # Watershed algorithm
+    markers = cv2.watershed(img, markers)
+    img[markers == -1] = [255, 0, 0]  # Mark the boundaries
+
+    # Extract the segmented image using the threshold mask
+    segmented_image = img.copy()  # Start with a copy of the original image
+    segmented_image[thresh == 0] = [0, 0, 0]  # Set the background to black
+
+    return (thresh, segmented_image)
